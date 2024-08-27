8532a01d1bbae395488ee1b8c3083e00dadfe47f
hedrobyte <pedrohrmalves@gmail.com>
2024-08-27 18:16:10 -0300

    feat: add GET funtion

---
+++ src/app/customer-manager/customer-manager.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { CustomerManagerService } from '../services/customer-manager.service';
+import { Customer } from '../models/customer.model';
+import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-customer-manager',
+  standalone: true,
+  imports: [DatePipe],
+  templateUrl: './customer-manager.component.html',
+  styleUrl: './customer-manager.component.css'
+})
+export default class CustomerManagerComponent implements OnInit{
+  customers: Customer[] = [];
+
+  constructor(private customerService: CustomerManagerService) {}
+
+  ngOnInit(): void {
+    this.customerService.getCustomers()
+    .subscribe((customer => {
+      this.customers = customer;
+    }))
+  }
+
+}
---
+++ src/app/models/customer.model.ts
@@ -0,0 +1,7 @@
+export interface Customer {
+    id: number;
+    name: string;
+    cpf: string;
+    dateOfBirth: string; // Date em formato ISO 8601
+    email: string;
+}
